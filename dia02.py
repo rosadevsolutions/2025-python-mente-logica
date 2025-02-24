f7dd1e5d0ae173064cc112926ee3ad9194467d94
Rosadev Solutions | Dev Romulo Rosa <rosadevsolutions@gmail.com>
2025-02-24 12:05:00 -0300

    dia 02 exercícios ebook

--- dia02.py
+++ dia02.py
@@ -1,5 +1,40 @@
-calc01  = 2 + 2
-calc02 = 10 * 5
+# EXERCÍCIO 01 - DECLARANDO VARIAVÉIS
+# Criar um programa que declare as seguintes variáveis:
+#  nome: seu nome.
+# idade: sua idade.
+# altura: sua altura.
+# estudante:se você é estudante ou não (true/false).
 
-print(calc01)
-print(calc02)
+
+# EXERCÍCIO 02 - EXIBINDO INFORMAÇÕES
+# Utilizar a função print() para exibir as informações das variáveis criadas no exercício anterior.
+
+
+# EXERCÍCIO 03 - OPERAÇÕES SIMPLES
+# Calcular o ano de nascimento com base na idade (considerando que o ano atual é 2025).
+# Verificar se a pessoa é maior de idade (idade >= 18) e armazenar o resultado numa variável booleana.
+
+
+# EXERCÍCIO 04 - MANIPULAÇÃO DE STRINGS
+# Criar uma variável frase que contenha a mensagem: "Olá, meu nome é [seu nome] e eu tenho [sua idade] anos."
+# DICA: Usar str(idade) para converter o número inteiro em string antes de concatenar.
+
+
+# EXERCÍCIO 05 - CALCULADORA SIMPLES
+# Pedir ao usuário para inserir dois números e armazenar em variáveis.
+# Calcular a soma, subtração, multiplicação e divisão desses números.
+# Exibir os resultados.
+# OBS: A função input() permite que o usuário insira dados.
+# Usamos float() para converter a entrada em um número de ponto flutuante.
+
+
+# DESAFIOS
+
+# 1) CONVERSOR DE TEMPERATURAS
+# Criar um programa que converta uma temperatura de graus Celsius para Fahrenheit.
+# Fórmula: F = C * 9/5 + 32
+
+# 2) ÁREA DE UM CÍRCULO
+# Criar um programa que calcule a área de um círculo com base no raio fornecido pelo usuário.
+# Fórmula: Área = π * raio²
+# π = 3.14159
