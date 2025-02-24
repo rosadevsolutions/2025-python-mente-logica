88a44ed3d74066eab4b34e4b2d451ae827d232b6
Rosadev Solutions | Dev Romulo Rosa <rosadevsolutions@gmail.com>
2025-02-24 11:50:55 -0300

    dia 01 resolucao exercicios ebook

--- dia01.py
+++ dia01.py
@@ -1,3 +1,26 @@
-# Comentários - Feitos com tralha
-# print - Comando para exibir o argumento(o que está erntre parenteses)
-print("Hello world")
+# Modificar o arquivo dia1.py para exibir uma mensagem de
+# Boas-vindas personalizada com seu nome.
+
+# ================================================================
+
+
+# EXERCÍCIO 2 EBOOK
+# 1) Realizar cálculos simples, como 2 + 2 ou 10 * 5.
+# 2) Imprimir outras mensagens usando print("Sua mensagem aqui")
+
+# MINHA RESPOSTA
+calc01 = 2 + 2
+calc02 = 10 * 5
+
+print(calc01)
+print(calc02)
+
+# ================================================================
+
+
+# EXERCÍCIO 03 EBOOK
+# Exibir mensagem de boas-vindas personalizada com seu nome
+
+# MINHA RESPOSTA
+nome = 'Romulo'
+print(f'{nome} seja bem-vindo!')
