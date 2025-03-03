0670d23c7d26e7aa75b9c9f01bef2eb7ea35a36c
Rosadev Solutions | Dev Romulo Rosa <rosadevsolutions@gmail.com>
2025-03-03 17:29:43 -0300

    dia04 lista de exercicios

---
+++ dia04.py
@@ -0,0 +1,38 @@
+# EXERCÍCIOS E-BOOK
+
+# 1) NÚMERO POSITIVO, NEGATIVO OU ZERO
+# Solicitar um número ao usuário e verificar se é positivo, negativo ou zero.
+
+# 2) CALCULADORA SIMPLES
+# Pedir ao usuário dois números e uma operação (+,-, *, /) e realizar o cálculo correspondente.
+
+# 3) CLASSIFICAÇÇÃO DE IDADE
+# Criar um programa que classifica a idade de uma pessoa em:
+# ● Criança: 0 a 12 anos
+# ● Adolescente: 13 a 17 anos
+# ● Adulto: 18 a 59 anos
+# ● Idoso: 60 anos ou mais
+
+# 4) ANO BISSEXTO
+# Verificar se um ano é bissexto.
+# ● Um ano é bissexto se for divisível por 4.
+# ● Não é bissexto se for divisível por 100, exceto se for divisível por 400.
+
+# 5) CAIXA ELETRÔNICO
+# O usuário deve informar o valor do saque (apenas valores inteiros)
+# O programa deve informar quantas cédulas de cada valor serão fornecidas.
+# Considere cédulas de R$100, R$50, R$20, R$10, R$5 e R$2.
+
+# DESAFIOS EXTRAS
+
+# 1) EMPRÉSTIMO BANCÁRIO
+# Criar programa para analisar o pedido de empréstimo.
+# O cliente deve informar o valor do empréstimo, renda mensal e número de parcelas.
+# O empréstimo será aprovado se o valor da parcela não exceder 30% da renda mensal.
+
+# 2) PEDRA, PAPEL OU TESOURA
+# Simular o jogo "Pedra, Papel ou Tesoura" entre o usuário e o computador.
+
+# 3) CALCULADORA DE TARIFA DE TÁXI
+# Uma empresa de táxi cobra uma tarifa básica de R$4.00, mais R$0.25 por quilômetro rodado.
+# Calcular o valor total da corrida com base na distância percorrida.
