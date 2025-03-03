311bbf267888bafc7604bc01e92fe95fe0a11ee0
Rosadev Solutions | Dev Romulo Rosa <rosadevsolutions@gmail.com>
2025-03-03 17:39:29 -0300

    dia04 conclusao exercicio 01

--- dia04.py
+++ dia04.py
@@ -2,6 +2,24 @@
 
 # 1) NÚMERO POSITIVO, NEGATIVO OU ZERO
 # Solicitar um número ao usuário e verificar se é positivo, negativo ou zero.
+print(f'\n=====================================')
+print(f'1) NÚMERO POSITIVO, NEGATIVO OU ZERO - MINHA RESPOSTA\n')
+
+in_numero_usuario = int(input("Informe um número: \n"))
+
+proc_numero_negativo = in_numero_usuario < 0
+proc_numero_positivo = in_numero_usuario > 0
+
+out_mensagem = ''
+
+if proc_numero_negativo:
+  out_mensagem = 'negativo'
+elif proc_numero_positivo:
+  out_mensagem = 'positivo'
+else:
+  out_mensagem = 'igual a zero'
+
+print(f'\n{in_numero_usuario} é um número {out_mensagem}.\n\n')
 
 # 2) CALCULADORA SIMPLES
 # Pedir ao usuário dois números e uma operação (+,-, *, /) e realizar o cálculo correspondente.
