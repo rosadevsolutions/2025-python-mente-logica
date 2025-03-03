452daacd316cee2f79e6dc21b25e1aa20c525710
Rosadev Solutions | Dev Romulo Rosa <rosadevsolutions@gmail.com>
2025-03-03 18:12:43 -0300

    dia04 conclusao exercicio 02

--- dia04.py
+++ dia04.py
@@ -23,6 +23,33 @@ print(f'\n{in_numero_usuario} é um número {out_mensagem}.\n\n')
 
 # 2) CALCULADORA SIMPLES
 # Pedir ao usuário dois números e uma operação (+,-, *, /) e realizar o cálculo correspondente.
+print(f'\n=====================================')
+print(f'2) CALCULADORA SIMPLES - MINHA RESPOSTA\n')
+
+in_numero_1 = int(input("Informe um número: \n"))
+in_numero_2 = int(input("Informe outro número: \n"))
+in_operacao = input("Informe a operação a ser realizada com os números: \n")
+in_operacao = in_operacao.lower()
+
+proc_calc_somar = in_numero_1 + in_numero_2
+proc_calc_subtrair = in_numero_1 - in_numero_2
+proc_calc_multiplicar = in_numero_1 * in_numero_2
+proc_calc_dividir = in_numero_1 / in_numero_2
+
+out_mensagem = ''
+
+if in_operacao == "somar":
+  out_mensagem = f'{in_numero_1} + {in_numero_2} = {proc_calc_somar}'
+elif in_operacao == "subtrair":
+  out_mensagem = f'{in_numero_1} - {in_numero_2} = {proc_calc_subtrair}'
+elif in_operacao == "multiplicar":
+  out_mensagem = f'{in_numero_1} x {in_numero_2} = {proc_calc_multiplicar}'
+elif in_operacao == "dividir":
+  out_mensagem = f'{in_numero_1} / {in_numero_2} = {proc_calc_dividir:.2f}'
+else:
+  out_mensagem = 'Informe uma operação válida'
+
+print(out_mensagem)
 
 # 3) CLASSIFICAÇÇÃO DE IDADE
 # Criar um programa que classifica a idade de uma pessoa em:
