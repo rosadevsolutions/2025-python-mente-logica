3f96f718b00f97e8c14d05c9a7ba0515fb172947
Rosadev Solutions | Dev Romulo Rosa <rosadevsolutions@gmail.com>
2025-03-04 17:05:53 -0300

    dia04 conclusao desafio 01

--- dia04.py
+++ dia04.py
@@ -96,7 +96,29 @@ print(f'4) ANO BISSEXTO\n')
 # Criar programa para analisar o pedido de empréstimo.
 # O cliente deve informar o valor do empréstimo, renda mensal e número de parcelas.
 # O empréstimo será aprovado se o valor da parcela não exceder 30% da renda mensal.
+print(f'\n=====================================')
+print(f'DESAFIO 1) EMPRÉSTIMO BANCÁRIO\n')
 
+in_emprestimo_valor = float(input('Valor do empréstimo desejado: \n'))
+in_renda_mensal = float(input('\nSua renda mensal: \n'))
+in_emprestimo_parcelas = float(input('\nQuantidade de parcelas desejadas: \n'))
+
+in_emprestimo_valor = in_emprestimo_valor
+in_renda_mensal = in_renda_mensal
+
+proc_emprestimo_parcela_valor = in_emprestimo_valor / in_emprestimo_parcelas
+proc_emprestimo_parcela_proporcao_renda = (proc_emprestimo_parcela_valor / in_renda_mensal) * 100
+proc_cond_emprestimo_liberacao = proc_emprestimo_parcela_proporcao_renda <= 30
+
+out_emprestimo_status = 'Empréstimo Negado.'
+out_comprometimento = f'Parcela compromete {proc_emprestimo_parcela_proporcao_renda}% de sua renda mensal.'
+
+if(proc_cond_emprestimo_liberacao):
+  out_emprestimo_status = 'Empréstimo Aprovado.'
+  out_comprometimento = f'Parcela compromete apenas {proc_emprestimo_parcela_proporcao_renda}% de sua renda mensal'
+
+out_mensagem = f'\n{out_emprestimo_status}\n{out_comprometimento}'
+print(out_mensagem)
 
 # 2) PEDRA, PAPEL OU TESOURA
 # Simular o jogo "Pedra, Papel ou Tesoura" entre o usuário e o computador.
