4382e0a9fce6b4a18d5f242b957d5c264f1517e7
Rosadev Solutions | Dev Romulo Rosa <rosadevsolutions@gmail.com>
2025-03-06 20:41:34 -0300

    dia04 conclusao exercicio 06

--- dia04-exercicio06.py
+++ dia04-exercicio06.py
@@ -10,3 +10,28 @@
 print('\n=====================================')
 print('6) SEMÁFORO - MINHA RESPOSTA')
 print('=====================================\n')
+
+in_sinal = input('Informe a cor do semáforo: \n').upper()
+
+cond_amarelo = in_sinal == 'AMARELO'
+cond_verde = in_sinal == 'VERDE'
+cond_vermelho = in_sinal == 'VERMELHO'
+cond_sinal_invalido = (not cond_amarelo) and (not cond_verde) and (not cond_vermelho)
+
+out_sinal = in_sinal.capitalize()
+out_status = ''
+out_mensagem = ''
+
+if cond_sinal_invalido:
+  out_mensagem = 'COR INVÁLIDA. Escolha entre verde, vermelho ou amarelo.'
+else:
+  if cond_amarelo:
+    out_status = 'ATENÇÃO! O motorista pode passar, mas precisa dar preferência ao pedestre.'
+  elif cond_vermelho:
+    out_status = 'PARE! O sinal está fechado para o motorista.'
+  else:
+    out_status = 'PASSE! O sinal está aberto para o motorista.'
+
+  out_mensagem = f'\nSinal {out_sinal} | {out_status}'
+
+print(out_mensagem)
